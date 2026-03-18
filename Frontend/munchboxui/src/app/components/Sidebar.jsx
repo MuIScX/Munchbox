9875c22a161a6eecade4ef97553fa0596c0bc414
Pattakorn Kiatsupapong <148467207+kornkorn25@users.noreply.github.com>
2026-03-18 13:46:33 +0700

    revert sidebar order

--- Frontend/munchboxui/src/app/components/Sidebar.jsx
+++ Frontend/munchboxui/src/app/components/Sidebar.jsx
@@ -101,16 +101,18 @@ export default function Sidebar() {
       title: 'QUICK ACCESS',
       items: [
         { name: 'Dashboard', path: '/dashboard', icon: Home, color: 'text-orange-500' },
+        { name: 'View Reports', path: '/reports/', icon: FileChartColumn, color: 'text-emerald-500' },
+        { name: 'Manage Inventory', path: '/manageinventory/', icon: Package, iconActive: PackageOpen, color: 'text-blue-500' },
       ],
     },
     {
       title: 'SERVICE',
       items: [
-        { name: 'View Reports', path: '/reports', icon: FileChartColumn, color: 'text-emerald-500' },
-        { name: 'Manage Inventory', path: '/manageinventory', icon: Package, iconActive: PackageOpen, color: 'text-blue-500' },
         { name: 'Manage Staff', path: '/managestaff', icon: UserCircle, color: 'text-orange-500' },
         { name: 'Predict Ingredients', path: '/predict', icon: LineChart, color: 'text-emerald-500' },
         { name: 'Manage Recipe', path: '/managemenu', icon: Utensils, iconActive: UtensilsCrossed, color: 'text-purple-500' },
+        { name: 'Manage Inventory', path: '/manageinventory', icon: Package, iconActive: PackageOpen, color: 'text-blue-500' },
+        { name: 'View Reports', path: '/reports', icon: FileChartColumn, color: 'text-emerald-500' },
         { name: 'Accuracy Report', path: '/accuracy', icon: Target, color: 'text-emerald-500' },
         { name: 'Inventory Log', path: '/inventorylog', icon: ClipboardClock, color: 'text-yellow-500' },
       ],
